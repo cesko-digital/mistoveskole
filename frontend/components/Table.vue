af3b939cfa5f77c82275b15efedc44cda196680f
David Veselý <jsem@davidvesely.cz>
2022-04-08 12:42:19 +0200

    Table: use new source and columns

--- frontend/components/Table.vue
+++ frontend/components/Table.vue
@@ -5,7 +5,7 @@
       ref="Table"
       :columns="columns"
       :table-data="tableData"
-      row-key-field-name="ico"
+      row-key-field-name="Id"
       :max-height="600"
       :virtual-scroll-option="{
         enable: true,
@@ -28,46 +28,166 @@ export default {
       },
       columns: [
         {
-          field: 'name',
-          key: 'name',
-          title: 'Name',
+          field: 'Id',
+          key: 'Id',
+          title: 'Id',
           sortBy: '',
-          align: 'left',
-          width: '40%',
         },
         {
-          field: 'okres',
-          key: 'okres',
-          title: 'Okres',
+          field: 'Id Jazyk',
+          key: 'Id Jazyk',
+          title: 'Jazyk',
           sortBy: '',
-          align: 'left',
-          width: '15%',
         },
         {
-          field: 'ico',
-          key: 'ico',
-          title: 'ICO',
-          align: 'left',
+          field: 'Id Reditelstvi Id Okres Id Kraj Jmeno Cz',
+          key: 'Id Reditelstvi Id Okres Id Kraj Jmeno Cz',
+          title: 'Kraj',
           sortBy: '',
-          width: '15%',
         },
         {
-          field: 'schools',
-          key: 'schools',
-          title: 'Kapacity',
-          align: 'left',
-          width: '30%',
-          renderBodyCell: ({ row, column, rowIndex }, h) => {
-              return (<RowSchools row={row} />);
-          },
+          field: 'Id Reditelstvi Id Okres Jmeno Cz',
+          key: 'Id Reditelstvi Id Okres Jmeno Cz',
+          title: 'Město',
+          sortBy: '',
         },
+        {
+          field: 'Id Reditelstvi Red Izo',
+          key: 'Id Reditelstvi Red Izo',
+          title: 'Id Reditelstvi Red Izo',
+          sortBy: '',
+        },
+        {
+          field: 'Id Reditelstvi Red Plny Nazev',
+          key: 'Id Reditelstvi Red Plny Nazev',
+          title: 'Nazev',
+          sortBy: '',
+        },
+        {
+          field: 'Id Skola Typ',
+          key: 'Id Skola Typ',
+          title: 'Typ',
+          sortBy: '',
+        },
+        {
+          field: 'Izo',
+          key: 'Izo',
+          title: 'Izo',
+          sortBy: '',
+        },
+        {
+          field: 'Kapacita Uk Volno Celkem',
+          key: 'Kapacita Uk Volno Celkem',
+          title: 'Kapacita Uk Volno Celkem',
+          sortBy: '',
+        },
+        {
+          field: 'Kontakt Email',
+          key: 'Kontakt Email',
+          title: 'Email',
+          sortBy: '',
+        },
+        {
+          field: 'Kontakt Jmeno',
+          key: 'Kontakt Jmeno',
+          title: 'Jmeno',
+          sortBy: '',
+        },
+        {
+          field: 'Kontakt Telefon',
+          key: 'Kontakt Telefon',
+          title: 'Telefon',
+          sortBy: '',
+        },
+        {
+          field: 'Kontakt Www',
+          key: 'Kontakt Www',
+          title: 'Www',
+          sortBy: '',
+        },
+        {
+          field: 'Misto Adresa1',
+          key: 'Misto Adresa1',
+          title: 'Adresa1',
+          sortBy: '',
+        },
+        {
+          field: 'Misto Adresa2',
+          key: 'Misto Adresa2',
+          title: 'Adresa2',
+          sortBy: '',
+        },
+        {
+          field: 'Misto Adresa3',
+          key: 'Misto Adresa3',
+          title: 'Adresa3',
+          sortBy: '',
+        },
+        {
+          field: 'Misto Ruian Kod',
+          key: 'Misto Ruian Kod',
+          title: 'Ruian',
+          sortBy: '',
+        },
+        {
+          field: 'Poznamka Cz',
+          key: 'Poznamka Cz',
+          title: 'Poznamka Cz',
+          sortBy: '',
+        },
+        {
+          field: 'Poznamka Uk',
+          key: 'Poznamka Uk',
+          title: 'Poznamka Uk',
+          sortBy: '',
+        },
+        {
+          field: 'Skola Plny Nazev',
+          key: 'Skola Plny Nazev',
+          title: 'Skola Plny Nazev',
+          sortBy: '',
+        },
+        // {
+        //   field: 'name',
+        //   key: 'name',
+        //   title: 'Name',
+        //   sortBy: '',
+        //   align: 'left',
+        //   width: '40%',
+        // },
+        // {
+        //   field: 'okres',
+        //   key: 'okres',
+        //   title: 'Okres',
+        //   sortBy: '',
+        //   align: 'left',
+        //   width: '15%',
+        // },
+        // {
+        //   field: 'ico',
+        //   key: 'ico',
+        //   title: 'ICO',
+        //   align: 'left',
+        //   sortBy: '',
+        //   width: '15%',
+        // },
+        // {
+        //   field: 'schools',
+        //   key: 'schools',
+        //   title: 'Kapacity',
+        //   align: 'left',
+        //   width: '30%',
+        //   renderBodyCell: ({ row, column, rowIndex }, h) => {
+        //       return (<RowSchools row={row} />);
+        //   },
+        // },
       ],
       tableData: [],
     };
   },
 
   async mounted() {
-    this.tableData = await this.$axios.$get('/skoly.json');
+    this.tableData = await this.$axios.$get('https://s3.eu-central-1.wasabisys.com/treevio/demo/export_zarizeni_2022_04_08_11_11_11.json');
   },
 
   methods: {
@@ -89,3 +209,9 @@ export default {
   },
 };
 </script>
+
+<style>
+.ve-table .ve-table-container table.ve-table-content thead.ve-table-header tr.ve-table-header-tr th.ve-table-header-th {
+  white-space: nowrap;
+}
+</style>
