2bf08ab2cdc165a8540db1705a3882a32122b676
Jan Stefanides <jan.stefanides@gmail.com>
2022-04-11 15:41:33 +0200

    entitylistener/trida: store values in history by date

--- backend/src/EntityListener/TridaListener.php
+++ backend/src/EntityListener/TridaListener.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Event\OnFlushEventArgs;
 use App\Entity\Trida;
+use App\Entity\TridaHistorieKapacity;
 use App\Entity\Zarizeni;
 use Symfony\Component\HttpFoundation\RequestStack;
 
@@ -13,7 +14,7 @@ class TridaListener
 {
     /**
      * note: only onflush supports changing of other entities
-     * @see https://www.doctrine-project.org/projects/doctrine-orm/en/latest/reference/events.html#onflush.
+     * @see https://www.doctrine-project.org/projects/doctrine-orm/en/latest/reference/events.html#onflush
      */
     public function onFlush(OnFlushEventArgs $args)
     {
@@ -30,15 +31,40 @@ class TridaListener
                 continue;
             }
 
+            // sum capacity into Zarizeni
             // TODO prevent call sumCapacity() for each trida again?
-            $zarizeniMetadata = $em->getClassMetadata(Zarizeni::class);
             $zarizeni = $entity->getIdZarizeni();
             $zarizeni->sumCapacity();
-            $em->persist($zarizeni);
-            $uow->recomputeSingleEntityChangeSet($zarizeniMetadata, $zarizeni);
+            $uow->persist($zarizeni);
+
+            // store daily history
+// FIXME works only for today, lookup $entity->getDatumCasAktualizace() in history first!
+            $historie = $entity->getHistorieKapacity();
+            if ($historie->count() == 0 || $historie->first()->getDatum() != (new \DateTime('midnight'))) {
+                // new row for today
+                $tridaHistorie = new TridaHistorieKapacity();
+                $tridaHistorie->setKapacitaUkVolno($entity->getAktualniKapacitaUkVolno())
+                    ->setKapacitaUkObsazeno($entity->getAktualniKapacitaUkObsazeno())
+                    ->setDatum($entity->getDatumCasAktualizace())
+                ;
+                $entity->addHistorieKapacity($tridaHistorie);
+                $uow->persist($tridaHistorie);
+                $tridaHistorieMetadata = $em->getClassMetadata(get_class($tridaHistorie));
+                $uow->computeChangeSet($tridaHistorieMetadata, $tridaHistorie);
+            } else {
+                // update for today
+                $tridaHistorie = $historie->first(); // collection must be sorted 
+                $tridaHistorie->setKapacitaUkVolno($entity->getAktualniKapacitaUkVolno())
+                    ->setKapacitaUkObsazeno($entity->getAktualniKapacitaUkObsazeno())
+                    ->setDatum($entity->getDatumCasAktualizace())
+                ;
+                $uow->persist($tridaHistorie);
+                $tridaHistorieMetadata = $em->getClassMetadata(get_class($tridaHistorie));
+                $uow->computeChangeSet($tridaHistorieMetadata, $tridaHistorie);
             }
-
-            // TODO store into tridahistorie too
+            $uow->persist($zarizeni);
+            $zarizeniMetadata = $em->getClassMetadata(get_class($zarizeni));
+            $uow->computeChangeSet($zarizeniMetadata, $zarizeni);
         }
-
+    }
 }
