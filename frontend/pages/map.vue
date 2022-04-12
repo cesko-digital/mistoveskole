5bf325054262e731ee6e754d8711885ae49572b3
David Veselý <jsem@davidvesely.cz>
2022-04-12 19:45:20 +0200

    Update map fill, use map component

---
+++ frontend/pages/map.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <h1 class="text-center text-3xl my-12">
+      Místo ve <strong>škole</strong>
+    </h1>
+    <DistrictsMap
+      @click="(district) => $router.push(`/?district=${district.title}`)"
+    />
+  </div>
+</template>
