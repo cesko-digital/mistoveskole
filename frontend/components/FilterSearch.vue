28a8d03adeab6059e89bf92b3a298bc8a6d3755c
David Veselý <jsem@davidvesely.cz>
2022-04-12 19:32:29 +0200

    Table: add text search filtering

---
+++ frontend/components/FilterSearch.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="p-2">
+    <input
+      v-model="searchValue"
+      type="search"
+      class="w-full"
+      placeholder="Hledat..."
+    >
+    <div class="ve-dropdown-operation">
+      <button
+        class="ve-dropdown-operation-item"
+        @click="$emit('search', ''), hide()"
+      >
+        Cancel
+      </button>
+      <button
+        class="ve-dropdown-operation-item"
+        @click="$emit('search', searchValue), hide()"
+      >
+        Search
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchValue: '',
+    };
+  },
+
+  methods: {
+    hide() {
+      document.querySelector('.ve-dropdown-dd-show').classList.remove('ve-dropdown-dd-show');
+    },
+  },
+};
+</script>
