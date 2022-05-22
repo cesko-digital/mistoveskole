931ca64434727655af14c4216875049597d2b0a7
quentincaffeino <sergey95zu@gmail.com>
2022-05-22 20:50:01 +0200

    display selected class number

--- frontend/pages/testFindAppropriateSchool.vue
+++ frontend/pages/testFindAppropriateSchool.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="container">
     <FindAppropriateSchool @selectionChanged="onSchoolSelected" />
+
+    <pre class="pt-4 font-bold text-red-500">Selected class: {{ classNumber }}</pre>
   </div>
 </template>
 
@@ -9,8 +11,15 @@ import FindAppropriateSchool from '../components/FindAppropriateSchool.vue';
 export default {
   components: { FindAppropriateSchool },
 
+  data() {
+    return {
+      classNumber: '0',
+    };
+  },
+
   methods: {
-    onSchoolSelected() {
+    onSchoolSelected({ classNumber }) {
+      this.classNumber = classNumber;
     },
   },
 };
