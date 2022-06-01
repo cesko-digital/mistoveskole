7932e84f60f81da273eb404d311d5cf4029901db
quentincaffeino <sergey95zu@gmail.com>
2022-06-01 21:50:15 +0200

    feat: port styleguide typography to tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,12 @@ const { default: colors } = require('./tailwind/colors');
 const textColor = require('./tailwind/textColor');
 const borderColor = require('./tailwind/borderColor');
 const backgroundColor = require('./tailwind/backgroundColor');
+const fontFamily = require('./tailwind/fontFamily');
+const fontSize = require('./tailwind/fontSize');
+const fontWeight = require('./tailwind/fontWeight');
+const letterSpacing = require('./tailwind/letterSpacing');
+const lineHeight = require('./tailwind/lineHeight');
+const typography = require('./tailwind/typography');
 
 module.exports = {
   content: [
@@ -21,9 +27,16 @@ module.exports = {
       textColor,
       borderColor,
       backgroundColor,
+      fontFamily,
+      fontSize,
+      fontWeight,
+      letterSpacing,
+      lineHeight,
+      typography,
     },
   },
   plugins: [
     require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
   ],
 };
