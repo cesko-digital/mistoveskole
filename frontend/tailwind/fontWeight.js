42815d483be8848720a02719debaa697d1b47797
Sergei ZH <sergey95zu@gmail.com>
2022-06-05 18:38:30 +0200

    feat: port styleguide typography to tailwind (#14)

---
+++ frontend/tailwind/fontWeight.js
@@ -0,0 +1,37 @@
+module.exports = {
+  headline: {
+    h1: 700,
+    h2: 400,
+    h3: 700,
+    h4: 400,
+    h5: 700,
+  },
+
+  overline: {
+    DEFAULT: 700,
+  },
+
+  body: {
+    DEFAULT: 400,
+
+    large: {
+      // DEFAULT: 400,
+
+      bold: {
+        DEFAULT: 700,
+      },
+    },
+
+    bold: {
+      DEFAULT: 700,
+    },
+
+    small: {
+      // DEFAULT: 400,
+    },
+
+    xsmall: {
+      // DEFAULT: 400,
+    },
+  },
+};
---
+++ frontend/tailwind/lineHeight.js
@@ -0,0 +1,37 @@
+module.exports = {
+  headline: {
+    h1: '64px',
+    h2: '56px',
+    h3: '48px',
+    h4: '40px',
+    h5: '32px',
+  },
+
+  overline: {
+    DEFAULT: '24px',
+  },
+
+  body: {
+    DEFAULT: '24px',
+
+    large: {
+      DEFAULT: '32px',
+
+      bold: {
+        // DEFAULT: '32px',
+      },
+    },
+
+    bold: {
+      // DEFAULT: '24px',
+    },
+
+    small: {
+      DEFAULT: '16px',
+    },
+
+    xsmall: {
+      DEFAULT: '16px',
+    },
+  },
+};
