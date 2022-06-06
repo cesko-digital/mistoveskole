b80ed0177797768a3ee377ea4eda3a220520ab12
Sergei ZH <sergey95zu@gmail.com>
2022-06-06 15:46:28 +0200

    feat sidebar (#20)

--- frontend/components/FindAppropriateSchool.vue
+++ frontend/components/FindAppropriateSchool.vue
@@ -134,7 +134,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .box {
   @apply flex flex-col space-y-s bg-muted p-l;
 }
---
+++ frontend/pages/testSidebar.vue
@@ -0,0 +1,13 @@
+<template>
+  <Sidebar />
+</template>
+
+<script>
+import Sidebar from '@/components/Sidebar.vue';
+
+export default {
+  components: {
+    Sidebar,
+  },
+};
+</script>
