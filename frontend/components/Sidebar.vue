80b65da362611436eca8aa4e97e2a20a434281f5
mvdegtyar <mv_degtyar@yahoo.com>
2022-07-25 15:32:30 +0300

    Add info link to the sidebar

--- frontend/components/Sidebar.vue
+++ frontend/components/Sidebar.vue
@@ -8,11 +8,31 @@
       </div>
 
       <FindAppropriateSchool />
+      <NuxtLink :to="localePath('how-the-czech-education-system-works')" target="_blank">
+        <div class="flex items-center">
+          <BookIcon class="m-s" />
+          <span class="grow">
+            {{ $t('components.Sidebar.how_the_Czech_education_system_works') }}
+          </span>
+          <ArrowIcon class="m-s" />
+        </div>
+      </NuxtLink>
     </div>
 
     <StandWithUkraine type="sidebar" />
   </div>
 </template>
+
+<script>
+import BookIcon from '~/assets/images/icons/book.svg?inline';
+import ArrowIcon from '~/assets/images/icons/right-arrow.svg?inline';
+export default {
+  components: {
+    BookIcon,
+    ArrowIcon,
+  },
+};
+</script>
 
 <style scoped>
 .sidebar {
