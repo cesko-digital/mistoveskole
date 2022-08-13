7e8a83411b3426093fac1447c3f63d4d37f8fd3d
mvdegtyar <mv_degtyar@yahoo.com>
2022-08-13 13:06:34 +0300

    Add info link to the sidebar (#61)
    Co-authored-by: quentincaffeino <sergey95zu@gmail.com>

--- frontend/components/Sidebar.vue
+++ frontend/components/Sidebar.vue
@@ -14,6 +14,26 @@
       </div>
 
       <FindAppropriateSchool />
+
+      <div class="p-m md:p-l">
+        <NuxtLink
+          :to="localePath('how-the-czech-education-system-works')"
+          target="_blank"
+          class="block py-xs rounded-outer hover:bg-sg-neutral-80"
+        >
+          <div class="flex items-center space-x-s pr-s">
+            <BookIcon />
+
+            <span class="font-body-bold">
+              {{
+                $t("components.Sidebar.how_the_Czech_education_system_works")
+              }}
+            </span>
+
+            <ArrowIcon class="m-xs" />
+          </div>
+        </NuxtLink>
+      </div>
     </div>
 
     <StandWithUkraine type="sidebar" />
@@ -23,7 +43,14 @@
 <script>
 import { mapMutations } from 'vuex';
 
+import BookIcon from '~/assets/images/icons/book.svg?inline';
+import ArrowIcon from '~/assets/images/icons/right-arrow.svg?inline';
+
 export default {
+  components: {
+    BookIcon,
+    ArrowIcon,
+  },
   data() {
     return {
       source: new URL(this.$config.mapoticUrl + '/?categories_ids=25972&q='),
