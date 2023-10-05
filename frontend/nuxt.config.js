2b65ccea412da869c617dda1848d79bdd3a4df77
jan stefanides <stefanides@users.noreply.github.com>
2023-10-05 14:58:05 +0200

    nuxt.config: redirect all pages

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -10,6 +10,7 @@ export default {
   head: {
     title: TITLE,
     meta: [
+      { 'http-equiv': 'refresh', content: '0; url=https://doskolyspolecne.cz/'},
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: DESCRIPTION },
